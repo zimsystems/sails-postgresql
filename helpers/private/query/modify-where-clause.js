a79a4487c2f08b82ad525b301d0c3604ea94f58e
Jan Houben <jan@et.tc>
2025-03-18 20:25:52 -0400

    convert  contraints to

--- helpers/private/query/modify-where-clause.js
+++ helpers/private/query/modify-where-clause.js
@@ -17,12 +17,12 @@
 var _ = require("@sailshq/lodash");
 
 module.exports = function modifyWhereClause(whereClause, meta) {
-  // Handle empty `where` clause and missing meta.
-  if (_.keys(whereClause).length === 0 || !meta) {
+  // Handle empty `where` clause
+  if (_.keys(whereClause).length === 0) {
     return whereClause;
   }
 
-  var makeLikeModifierCaseInsensitive = meta.makeLikeModifierCaseInsensitive;
+  var makeLikeModifierCaseInsensitive = meta && meta.makeLikeModifierCaseInsensitive;
 
   // Recursively modify the `where` clause.
   var queryFilter = (function recurse(branch) {
@@ -51,6 +51,12 @@ module.exports = function modifyWhereClause(whereClause, meta) {
     if (constraint.like && makeLikeModifierCaseInsensitive) {
       constraint.ilike = constraint.like;
       delete constraint.like;
+    }
+
+    // convert `!= null` to `is not null`
+    if (constraint['!='] === null) {
+      constraint['is not'] = null;
+      delete constraint['!='];
     }
 
     return branch;
